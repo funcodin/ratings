bb447f6a7bc98dab50bdba3c396516b24948effb
Rohit Dhumal <Rohit@rohits-air.home>
2015-04-05 18:27:46 -0400

    Updated git ignore Updated pom add new pojo

---
+++ ratings-type/src/main/java/com/multi/enterprise/ratings/types/Movie.java
@@ -0,0 +1,39 @@
+package com.multi.enterprise.ratings.types;
+
+import java.util.Date;
+
+public class Movie {
+
+	private int movieId;
+	private String movieName;
+	private Date createdDate;
+	private Date modifiedDate;
+	
+	public int getMovieId() {
+		return movieId;
+	}
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+	public String getMovieName() {
+		return movieName;
+	}
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+	
+	
+	
+}
